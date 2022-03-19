f39a61cd42ac82cd12d94e4c1065a4d0585d6a52
Code champ <anshuc52@gmail.com>
2022-03-19 17:34:58 +0530

    Lect1

---
+++ ExampleSet1/src/JavaBasicsExampleSet1.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class JavaBasicsExampleSet1 {
+
+	// JVM - Provides you the run time in which java bytecode can be executed
+	//       JVM is responsible to load, Verify, Executes the code.
+	//       can also run programs which are written in other lang but compiled to java byte code.
+	// 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		HashMap<Integer,Integer> map = new HashMap<>();
+
+	}
+
+}
